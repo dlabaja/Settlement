f433f18538354928cbb21d92599f82f625619354
dlabaja <dlabaja.jan@seznam.cz>
2026-02-17 22:06:52 +0100

    vector util

--- Assets/Scripts/Utils/VectorUtils.cs
+++ Assets/Scripts/Utils/VectorUtils.cs
@@ -10,4 +10,24 @@ public static class VectorUtils
                && vec1.y.Approx(vec2.y, eps) 
                && vec1.z.Approx(vec2.z, eps);
     }
+    
+    public static Vector3 Forward(this Transform transform)
+    {
+        return transform.forward;
+    }
+
+    public static Vector3 Backward(this Transform transform)
+    {
+        return -transform.forward;
+    }
+
+    public static Vector3 Right(this Transform transform)
+    {
+        return transform.right;
+    }
+
+    public static Vector3 Left(this Transform transform)
+    {
+        return -transform.right;
+    }
 }
